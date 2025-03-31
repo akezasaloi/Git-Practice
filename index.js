13ad76e64064aaefced0b709dba2cd7ac05cb73a
Akeza Saloi <akezasaloi@gmail.com>
2025-03-31 15:10:13 +0300

    Practiced manipulating strings using string methods

--- index.js
+++ index.js
@@ -12,17 +12,54 @@
 // let word1 = "Hello";
 // let word2 = "Javascript";
 // console.log(word1.concat(" ",word2));
-let str = "extravaganza";
-console.log(str.slice(8));
-const food = "The quick fox jumped over the lazy dog";
-console.log(food.slice(4,0,"eat"));
-// let food1 = food.slice(0,3);
-// let food2 = food.slice(4,);
-// console.log(food1 +" " + "eat" + " "+ food2);
-const story = "The quick brown fox jumped over the lazy dog";
-// console.log(story.count("the"));
-// console.log(story.count("brown"));
-let str1 = "The pupils are reading in library";
-console.log(str1.includes("are"));
-let word1 = "Wonderful";
-console.log (word1.to) 
+// let str = "extravaganza";
+// console.log(str.slice(8));
+// const food = "The quick fox jumped over the lazy dog";
+// console.log(food.slice(4,0,"eat"));
+// // let food1 = food.slice(0,3);
+// // let food2 = food.slice(4,);
+// // console.log(food1 +" " + "eat" + " "+ food2);
+// const story = "The quick brown fox jumped over the lazy dog";
+// // console.log(story.count("the"));
+// // console.log(story.count("brown"));
+// let str1 = "The pupils are reading in library";
+// console.log(str1.includes("are"));
+// let word1 = "Wonderful";
+// console.log (word1.toUpperCase) 
+// let string = "An undERneath world";
+// let splitedword = string.split(" ");
+// console.log(splitedword);
+// splitedword.forEach((item,index)=>{splitedword[index] = item.charAt(0).toUpperCase()+ item.slice(1).toLowerCase ()});
+// let finalword = splitedword.toString();
+// // let finalword = splitedword.join(" ");
+// console.log(finalword);
+// let veggies = ["Tomato","Spinach","cauliflower","cucumber","Tomato","cauliflower"];
+// // console.log(veggies.at(3));
+// console.log(veggies.indexOf("Spinach"));
+// console.log(veggies.lastIndexOf("cauliflower"));
+// console.log(veggies.push("Carrot"));
+// console.log(veggies);
+// console.log(veggies.pop());
+// console.log(veggies);
+// console.log(veggies.unshift("Celery"));
+// console.log(veggies);
+// console.log(veggies.shift());
+// console.log(veggies);
+// let vegan = [];
+// console.log(vegan.shift());
+// console.log(vegetables.splice(0,1,"spinach"));
+// console.log(vegetables);
+// let  vegetables = ["cucumber","Tomato","cauliflower","Tomato","Spinach"];
+// vegetables.fill("avocado",2,4);
+// console.log(vegetables);
+// vegetables.map((veggie)=>{vegetables.push("Mint")});
+let  vegetables = ["cucumber","tomato","cauliflower","4tomato","5spinach"];
+let vegetable = vegetables.map((veggie,index)=>
+ vegetables[index] = veggie.at(0).toUpperCase()+veggie.slice(1).toLowerCase()
+);
+console.log(vegetable);
+
+// let vegetable = vegetables.forEach((item,index)=>{
+//     vegetables[index] = Number(item.at(0)) + item.slice(1).toLocaleUpperCase()
+// });
+// console.log(vegetables); 
---
+++ indices.js
@@ -0,0 +1,27 @@
+// Find the last element of the following arrays.arr1 = [3,7,34,90,12]arr2 = [true, "green", "where",12,56]
+// Write a JS program that will join the following array elements into a string. myPets = ["Cow", "Bird", "Snake", "Dog"];
+// Write a JS script to sort the following array items. var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+// Write a program to remove duplicates from the following array. Console the array without duplicates, and console another array that only contains the duplicates
+// let arr = ["boy", "man", "girl",  "school", "girl", "woman"];
+// Write a JS script to search for the following word in the array."food"If the word is present, return it else return "the search word was not found". let arr5 = ["the", "way", "x", 4];
+// Write a JS script to sort the following string:let word = "renniw"
+// Using an array of fruits with length 10 , insert 'Tomato' at the 5th index 
+// let myPets = ["Cow", "Bird", "Snake", "Dog"];
+// console.log(myPets.join(" "));
+// var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+// console.log(arr3.sort());
+// let word = "renniw";
+// console.log(word.split("").sort().join(""));
+// let arr5= ["the", "way", "x", 4,"food"];
+// let searchWord = "food"
+// let ann=arr5.includes(searchWord)?searchWord:'The search was not found';
+// console.log(ann);
+// let arr = ["boy", "man", "girl",  "school", "girl", "woman"];
+// let unique = arr.filter((item,index)=>{
+//     return arr.indexOf(item) === index;
+// });
+// console.log(unique);
+// let duplicates = arr.filter((item,index)=>{
+//     return arr.indexOf(item)!== index;
+// });
+// console.log(duplicates);

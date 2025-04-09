c542c11050b9e03bd5914ec46550ef3579ffd5d9
Akeza Saloi <akezasaloi@gmail.com>
2025-04-09 22:08:45 +0300

    practice on forms

--- index.js
+++ index.js
@@ -53,13 +53,39 @@
 // vegetables.fill("avocado",2,4);
 // console.log(vegetables);
 // vegetables.map((veggie)=>{vegetables.push("Mint")});
-let  vegetables = ["cucumber","tomato","cauliflower","4tomato","5spinach"];
-let vegetable = vegetables.map((veggie,index)=>
- vegetables[index] = veggie.at(0).toUpperCase()+veggie.slice(1).toLowerCase()
-);
-console.log(vegetable);
-
+// let  vegetables = ["cucumber","tomato","cauliflower","4tomato","5spinach"];
+// let vegetable = vegetables.map((veggie,index)=>
+//  vegetables[index] = veggie.at(0).toUpperCase()+veggie.slice(1).toLowerCase()
+// );
+// console.log(vegetable);
 // let vegetable = vegetables.forEach((item,index)=>{
 //     vegetables[index] = Number(item.at(0)) + item.slice(1).toLocaleUpperCase()
 // });
 // console.log(vegetables); 
+// Find the last element of the following arrays.arr1 = [3,7,34,90,12]arr2 = [true, "green", "where",12,56]
+// Write a JS program that will join the following array elements into a string. myPets = ["Cow", "Bird", "Snake", "Dog"];
+// Write a JS script to sort the following array items. var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+// Write a program to remove duplicates from the following array. Console the array without duplicates, and console another array that only contains the duplicates
+// let arr = ["boy", "man", "girl",  "school", "girl", "woman"];
+// Write a JS script to search for the following word in the array."food"If the word is present, return it else return "the search word was not found". let arr5 = ["the", "way", "x", 4];
+// Write a JS script to sort the following string:let word = "renniw"
+// Using an array of fruits with length 10 , insert 'Tomato' at the 5th index 
+// let myPets = ["Cow", "Bird", "Snake", "Dog"];
+// console.log(myPets.join(" "));
+// var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+// console.log(arr3.sort());
+// let word = "renniw";
+// console.log(word.split("").sort().join(""));
+// let arr5= ["the", "way", "x", 4,"food"];
+// let searchWord = "food"
+// let ann=arr5.includes(searchWord)?searchWord:'The search was not found';
+// console.log(ann);
+// let arr = ["boy", "man", "girl",  "school", "girl", "woman"];
+// let unique = arr.filter((item,index)=>{
+//     return arr.indexOf(item) === index;
+// });
+// console.log(unique);
+// let duplicates = arr.filter((item,index)=>{
+//     return arr.indexOf(item)!== index;
+// });
+// console.log(duplicates);
